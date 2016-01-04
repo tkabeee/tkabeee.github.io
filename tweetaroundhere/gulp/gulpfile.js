bdd704d3d38fbdb6c0c0dd9a3c5d83b78dae6f71
tkabeee <tkabee@gmail.com>
2016-01-05 00:22:42 +0900

    gulp + reactify + browserifyでjsxを自動ビルド

--- tweetaroundhere/gulp/gulpfile.js
+++ tweetaroundhere/gulp/gulpfile.js
@@ -1,9 +1,18 @@
+"use strict";
+
 var gulp = require("gulp");
 var webserver = require('gulp-webserver');
 var plumber = require("gulp-plumber");
-// var sass = require("gulp-sass");
 var compass = require("gulp-compass");
 var autoprefixer = require("gulp-autoprefixer");
+
+var duration = require('gulp-duration');
+var util = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var glob = require('glob');
 
 var SCSS_FILE = "sass/*.scss";
 var SASS_DIR = "../sass"
@@ -26,17 +35,42 @@ gulp.task("compass", function(){
     ;
 });
 
-// gulp.task("sass", function(){
-//   gulp.src([SCSS_FILE])
-//       .pipe(plumber())
-//       .pipe(sass({
-//         outputStyle: 'expanded',
-//         compass: true
-//       }))
-//       .pipe(autoprefixer())
-//       .pipe(gulp.dest(CSS_DIR))
-//       ;
-// });
+var path_react = {
+  OUT: "build.js",
+  DEST_BUILD: "../js/build",
+  ENTRY_POINT: glob.sync('src/*.jsx')
+};
+
+var props_react = {
+  entries: path_react.ENTRY_POINT,
+  transform: [reactify],
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true,
+};
+
+var bundler = watchify(browserify(props_react));
+
+bundler.on("update", jsxCompiile);
+
+gulp.task("watchify", jsxCompiile);
+
+function jsxCompiile(){
+  return bundler.bundle()
+    .on("error", function(err){
+      console.log(util.colors.red("Oops! you have ERROR! \n" + err.message));
+      this.emit('end');
+    })
+    .pipe(source(path_react.OUT))
+    .pipe(duration('compiled "' + path_react.OUT + '"'))
+    .pipe(gulp.dest(path_react.DEST_BUILD))
+    ;
+}
+
+gulp.task("watch", function(){
+  gulp.watch(["../" + SCSS_FILE], ["compass"]);
+});
 
 gulp.task("webserver", function(){
   gulp.src('../../tweetaroundhere')
@@ -46,11 +80,8 @@ gulp.task("webserver", function(){
     ;
 });
 
-gulp.task("watch", function(){
-  gulp.watch(["../" + SCSS_FILE], ["compass"]);
-});
-
 gulp.task("default", function(){
   gulp.start('watch');
-  gulp.start('webserver');
+  gulp.start('watchify');
+  // gulp.start('webserver');
 });
