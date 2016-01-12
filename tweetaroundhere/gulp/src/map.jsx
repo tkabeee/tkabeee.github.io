4e2d27b5f5759ce044098a64ffcfce7370c6fe2d
tkabeee <tkabee@gmail.com>
2016-01-12 22:44:00 +0900

    マーカーとサークルの作成＆更新

--- tweetaroundhere/gulp/src/map.jsx
+++ tweetaroundhere/gulp/src/map.jsx
@@ -21,23 +21,7 @@ var MapBox = React.createClass({
     };
   },
   _initMap: function() {
-
-    // TDDO: document.ready(function(){}) を移設
-
-    //   Tws.setSearchItem();
-    //   Tws.setDistance();
-
-    // Tws.query  = (Tws.query)  ? Tws.query  : "";
-    // Tws.lat    = (Tws.lat)    ? Tws.lat    : 35.689488;
-    // Tws.lng    = (Tws.lng)    ? Tws.lng    : 139.691706;
-    // Tws.within = (Tws.within) ? Tws.within : 20;
-    // Tws.units  = (Tws.units)  ? Tws.units  : "km";
-    // Tws.rpp    = (Tws.rpp)    ? Tws.rpp    : 20;
-    // var Zoom   = (this.zoom)  ? this.zoom  : 10;
-    // var LatLng = new google.maps.LatLng(Tws.lat, Tws.lng);
-
-    // React error avoid
-    //Tws.setGeocode(Tws.lat, Tws.lng);
+    var self = this;
     this.gm = {};
     this.gm.map = new google.maps.Map(document.getElementById("mapCanvas"), {
       zoom:              this.state.zoom,
@@ -58,37 +42,37 @@ var MapBox = React.createClass({
 
     this.gm.geocoder = new google.maps.Geocoder();
     this.gm.infoWindow = new google.maps.InfoWindow();
-    this._updateMarkerPosition(this.gm.marker.getPosition());
-    this._geocodePosition(this.gm.marker.getPosition());
-    // TODO: まだ作成できない
-    // this._createCircle();
+    this._updateMarkerPosition();
+    this._geocodePosition();
+    this._createCircle();
     this._setDragEvent();
 
-    google.maps.event.addListener(this.gm.map, 'click', function(e) {
+    google.maps.event.addListener(self.gm.map, 'click', function(e) {
       var lat = e.latLng.lat();
       var lng = e.latLng.lng();
 
-      this._deleteMarker(this.gm.marker);
-      this._setMarker(lat, lng);
-      this._setDragEvent();
-      this._updateMarkerPosition(this.gm.marker.getPosition());
+      self._deleteMarker();
+      self._setMarker(lat, lng);
+      self._setDragEvent();
+      self._updateMarkerPosition();
 
-      if (this.circle) {
-        this._deleteCircle();
+      if (self.gm.circle) {
+        self._deleteCircle();
       }
 
-      this._createCircle();
+      self._createCircle();
       // TODO: 初期検索を実行
+      console.log('TODO:');
       // this.twsearch(Tws.query, lat, lng, Tws.within, Tws.units, Tws.rpp);
     });
 
-    google.maps.event.addListener(this.gm.map, 'zoom_changed', function() {
-      this._updateZoomLevel(this.gm.map.getZoom());
+    google.maps.event.addListener(self.gm.map, 'zoom_changed', function() {
+      self._updateZoomLevel(self.gm.map.getZoom());
     });
   },
   _moveCenter: function(lat,lng) {
-    var latlng = new google.maps.LatLng(lat,lng);
-    this.gm.map.setCenter(latlng);
+    var latLng = new google.maps.LatLng(lat,lng);
+    this.gm.map.setCenter(latLng);
   },
   _openInfoWindow: function() {
     // this.tweet is undefined
@@ -98,10 +82,10 @@ var MapBox = React.createClass({
   _closeInfoWindow: function() {
     this.gm.infoWindow.close();
   },
-  _geocodePosition: function(pos) {
+  _geocodePosition: function() {
     var self = this;
-    this.gm.geocoder.geocode({
-      latLng: pos
+    self.gm.geocoder.geocode({
+      latLng: self.gm.marker.getPosition()
     }, function(responses) {
       if (responses && responses.length > 0) {
         self._updateMarkerAddress(responses[0].formatted_address);
@@ -116,21 +100,22 @@ var MapBox = React.createClass({
       self._updateMarkerAddress("Address 取得中…");
     });
     google.maps.event.addListener(self.gm.marker, 'drag', function() {
-      self._updateMarkerPosition(self.gm.marker.getPosition());
-      self._updateZoomLevel(self.gm.map.getZoom());
+      self._updateMarkerPosition();
+      self._updateZoomLevel();
     });
     google.maps.event.addListener(self.gm.marker, 'dragend', function() {
       self._geocodePosition(self.gm.marker.getPosition());
       // TODO: マーカードラッグ後にtweet検索
+      console.log('TODO:');
       // this.twsearch(Tws.query, Tws.lat, Tws.lng, Tws.within, Tws.units, Tws.rpp);
     });
   },
   // ズームレベルを更新
-  _updateZoomLevel: function(level) {
-    // TODO: reject jquery
-    $("#zoom").val(level);
+  _updateZoomLevel: function() {
+    this.state.zoom = this.gm.map.getZoom();
   },
   _createMarker: function(lat,lng) {
+    var self = this;
     this.gm.icon = new google.maps.MarkerImage(
       'img/markers_twitter.png',
         new google.maps.Size(20,34),
@@ -149,15 +134,14 @@ var MapBox = React.createClass({
       shadow: this.gm.shadow,
       position: new google.maps.LatLng(lat,lng)
     });
-    google.maps.event.addListener(this.markers, 'click', function() {
-      this._openInfoWindow();
+    google.maps.event.addListener(self.gm.markers, 'click', function() {
+      self._openInfoWindow();
     });
-    google.maps.event.addListener(this.gm.map, 'click', function() {
-      this._closeInfoWindow();
+    google.maps.event.addListener(self.gm.map, 'click', function() {
+      self._closeInfoWindow();
     });
   },
   _setMarker: function(lat,lng) {
-    // var LatLng = new google.maps.LatLng(lat, lng);
     this.gm.marker = new google.maps.Marker({
       map: this.gm.map,
       position: new google.maps.LatLng(lat, lng),
@@ -165,33 +149,27 @@ var MapBox = React.createClass({
     });
     this._geocodePosition(this.gm.marker.getPosition());
   },
-  _deleteMarker: function(marker) {
-    marker.setMap(null);
+  _deleteMarker: function() {
+    this.gm.marker.setMap(null);
   },
   // マーカーのポジション更新
-  _updateMarkerPosition: function(latLng) {
-    // $("#lat").text(latLng.lat().toFixed(6));
-    // $("#lng").text(latLng.lng().toFixed(6));
+  _updateMarkerPosition: function() {
+    var latLng = this.gm.marker.getPosition();
     this.state.lat = latLng.lat().toFixed(6);
     this.state.lng = latLng.lng().toFixed(6);
-    // reactエラー回避の為、一時コメントアウト
-    // Tws.setGeocode(latLng.lat().toFixed(6), latLng.lng().toFixed(6));
+    document.getElementById("lat").textContent = this.state.lat;
+    document.getElementById("lng").textContent = this.state.lng;
   },
   // マーカーの住所を更新
   _updateMarkerAddress: function(str) {
-    // $("#address").text(str);
+    document.getElementById("address").textContent = str;
   },
   _createCircle: function() {
-    // var Lat  = $("#lat").text();
-    // var Lng  = $("#lng").text();
-    var geos = $("#geocode").val().split(",");
-    var within = geos[2].match(/^[0-9]+/);
-
     this.gm.circle = new google.maps.Circle({
       center:        new google.maps.LatLng(this.state.lat, this.state.lng),
       fillColor:     '#ff4500',
       fillOpacity:   0.2,
-      radius:        within*1000,
+      radius:        this.state.within*1000,
       strokeColor:   '#ff4500',
       strokeOpacity: 1,
       strokeWeight:  1
