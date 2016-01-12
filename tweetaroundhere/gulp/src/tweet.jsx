dc0ab75a8fb6f8c24b68711b37133aca1ac4503b
tkabeee <tkabee@gmail.com>
2016-01-12 09:36:27 +0900

    とりあえずPropTypesの設定はおいておく

--- tweetaroundhere/gulp/src/tweet.jsx
+++ tweetaroundhere/gulp/src/tweet.jsx
@@ -4,34 +4,46 @@ var React = require('react');
 var ReactDOM = require("react-dom");
 
 var Tweet = React.createClass({
-  propTypes: {
-    user: React.PropType.shape({
-      name: React.propTypes.string,
-      screen_name: React.PropType.string,
-      profile_image_url: React.PropType.string
-    }),
-    id_str: React.PropTypes.string,
-    text: React.PropTypes.string,
-    created_at: React.PropTypes.string
+  // propTypes: {
+  //   user: React.PropType.shape({
+  //     name: React.propTypes.string,
+  //     screen_name: React.PropType.string,
+  //     profile_image_url: React.PropType.string
+  //   }),
+  //   id_str: React.PropTypes.string,
+  //   text: React.PropTypes.string,
+  //   created_at: React.PropTypes.string
+  // },
+  getInitialState: function() {
+    return {
+      user: {
+        profile_image_url: '',
+        name: '',
+        screen_name: ''
+      },
+      id_str: '',
+      test: '',
+      created_at: ''
+    };
   },
   render() {
     return(
-      <ol class="stream-list">
-        <li class="stream-list-item">
-          <div class="tweet">
-            <div class="content">
-              <div class="stream-item-header">
-                <a class="account-group" href="/{this.props.user.screen_name}">
-                  <img class="profile-avatar" src="{this.props.user.profile_image_url}"/>
-                  <strong class="fullname">{this.props.user.name}</strong><span class="username"><s>@</s><b>{this.props.user.screen_name}</b></span>
+      <ol className="stream-list">
+        <li className="stream-list-item">
+          <div className="tweet">
+            <div className="content">
+              <div className="stream-item-header">
+                <a className="account-group" href="/{this.state.user.screen_name}">
+                  <img className="profile-avatar" src={this.state.user.profile_image_url} />
+                  <strong className="fullname">{this.state.user.name}</strong><span className="username"><s>@</s><b>{this.state.user.screen_name}</b></span>
                 </a>
-                <small class="time">
-                  <a href="//twitter.com/{this.props.user.screen_name}/status/{this.props.id_str}" class="tweet-timestamp">
-                    <span class="timestamp">{this.props.created_at}</span>
+                <small className="time">
+                  <a href="//twitter.com/{this.state.user.screen_name}/status/{this.state.id_str}" className="tweet-timestamp">
+                    <span className="timestamp">{this.state.created_at}</span>
                   </a>
                 </small>
               </div>
-              <p class="tweet-text">{this.props.text}</p>
+              <p className="tweet-text">{this.state.text}</p>
             </div>
           </div>
         </li>
